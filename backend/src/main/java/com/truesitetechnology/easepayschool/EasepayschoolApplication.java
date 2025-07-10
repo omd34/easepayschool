09ba76a0fa7c5e56861c37c69d0c873a2c657b14
omd34 <danielotomo34@gmail.com>
2025-07-10 15:24:57 +0100

    ajout de la partie initial du bakcend

---
+++ backend/src/main/java/com/truesitetechnology/easepayschool/EasepayschoolApplication.java
@@ -0,0 +1,13 @@
+package com.truesitetechnology.easepayschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasepayschoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasepayschoolApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/truesitetechnology/easepayschool/EasepayschoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.truesitetechnology.easepayschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasepayschoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
