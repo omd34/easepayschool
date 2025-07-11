0bc7f102ecbf3b4a81f88a6dc74b7803a2def758
programmer12-creator <ndzanayvon@gmail.com>
2025-07-11 20:00:48 +0200

    Mon premier commit

---
+++ frontend/web/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import './bup.css';
+function App() {
+  return (
+    <div className="text-center">
+          <button className ="btn btn-primary ">
+               <h1>
+
+                   Easypayschool Front end
+              </h1>
+          </button>
+    </div>
+  );
+}
+
+export default App;
